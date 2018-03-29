12c703937d860e118411656b269e611ca657aedc
Rafael <36059806+rafraga@users.noreply.github.com>
2018-03-29 00:17:40 -0700

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,13 @@
 
     $("#submit").on("click", function () {
+
         var url = [];
         url.push("https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=c2117551737444c0993461e828428e15");
         var search = ($("#search_box").val()).toLowerCase().replace(" ", "+");
         var startdate = ($("#startdate_box").val()).toLowerCase().replace(" ", "+");
         var enddate = ($("#enddate_box").val()).toLowerCase().replace(" ", "+");
-        //gave input field the id of "numberofResults_box"
-        var resultNum = parseInt($("#numberofResults_box").val(), 10);
-        if (resultNum > 10) {
-            resultNum = 10;
-        }
+        var resultNum = parseInt($("#sel1").val());
+
         if (startdate) {
             url.push(("&start_date=" + startdate));
         }
@@ -21,18 +19,23 @@
         };
 
         full_url = url.join('')
-        console.log(full_url);
-        console.log(resultNum);
         $.ajax({
             url: full_url,
             method: 'GET',
         }).done(function (result) {
             var data_div = [];
-            for (let i = 0; i < resultNum; i++) {
+            for (var i = 0; i < resultNum; i++) {
                 data_div.push("<h4>" + result.response.docs[i].snippet + "</h4>");
                 data_div.push("<h5><a href='" + result.response.docs[i].web_url + "'>" + result.response.docs[i].web_url + "</h5></a>");
-                data_div.push("<h6>Source: " + result.response.docs[i].source + "</h6>");
-                data_div.push("<h6>" + result.response.docs[i].pub_date + "</h6><br>");
+                if (result.response.docs[i].byline) {
+                    data_div.push("<h6>" + result.response.docs[i].byline.original + "</h6>");
+                };
+                if (result.response.docs[i].pub_date) {
+                    data_div.push("<h6>" + (result.response.docs[i].pub_date).substring(0,10) + "</h6>");
+                };
+                data_div.push("<br>")
+                
+                
             };
             $("#display_results").html(data_div.join(''));
         });
@@ -47,4 +50,3 @@
         $("#display_results").empty();
 
     });
-
